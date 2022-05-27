5665c8d9f9771a6257eef6deb7b62d0152a4e911
tales-lopes-meli <105742741+tales-lopes-meli@users.noreply.github.com>
2022-05-27 12:37:01 -0300

    first exercise

--- main.go
+++ main.go
@@ -1,14 +1,32 @@
 package main
 
-import (
-	"fmt"
-	"time"
-)
+import "fmt"
 
-const (
-	MSG_HELLO = "Hello, %s\n"
-)
+type CustomError struct {
+	status int
+	msg    string
+}
+
+func (e *CustomError) Error() string {
+	return fmt.Sprintf("Error: %s with status %d\n", e.msg, e.status)
+}
+
+func salaryTest(salario int) (int, error) {
+	if salario < 15000 {
+		return 500, &CustomError{
+			status: 500,
+			msg:    fmt.Sprintf("Salário de %d é menor que 15000\n", salario),
+		}
+	}
+	return 200, nil
+}
 
 func main() {
-	fmt.Println(time.Now().UnixNano())
+	var salario int = 20000
+	status, err := salaryTest(salario)
+	if err != nil {
+		fmt.Printf("Error: %s\n", err.Error())
+	} else {
+		fmt.Println(status)
+	}
 }
