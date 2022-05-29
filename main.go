0ea9c094baedfc0989a6f0d826cbcd1352e615f0
tales-lopes-meli <105742741+tales-lopes-meli@users.noreply.github.com>
2022-05-29 18:44:54 -0300

    second exercise

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
 type CustomError struct {
 	status int
@@ -8,7 +11,7 @@ type CustomError struct {
 }
 
 func (e *CustomError) Error() string {
-	return fmt.Sprintf("Error: %s with status %d\n", e.msg, e.status)
+	return fmt.Sprintf("%s with status %d\n", e.msg, e.status)
 }
 
 func salaryTest(salario int) (int, error) {
@@ -21,9 +24,24 @@ func salaryTest(salario int) (int, error) {
 	return 200, nil
 }
 
+func salaryVerify(salario int) (int, error) {
+	if salario < 15000 {
+		return 500, errors.New(fmt.Sprintf("Salário de %d é bem menor que 15000 with status 500\n", salario))
+	}
+	return 200, nil
+}
+
 func main() {
-	var salario int = 20000
+	var salario int = 2000
 	status, err := salaryTest(salario)
+	if err != nil {
+		fmt.Printf("Error: %s\n", err.Error())
+	} else {
+		fmt.Println(status)
+	}
+
+	// EX2
+	status, err = salaryVerify(salario)
 	if err != nil {
 		fmt.Printf("Error: %s\n", err.Error())
 	} else {
