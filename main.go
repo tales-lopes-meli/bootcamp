d1fa5200f39eded7b395b98c76d04e6c2170bcd7
tales-lopes-meli <105742741+tales-lopes-meli@users.noreply.github.com>
2022-05-31 23:18:30 -0300

    setup

--- main.go
+++ main.go
@@ -1,15 +1,24 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"fmt"
+
+	"github.com/gin-gonic/gin"
+)
+
+func handler(c *gin.Context) {
+
+	fmt.Println(c.Param("id"))
+
+	c.JSON(200, gin.H{
+		"message": "Hello, World!",
+	})
+}
 
 func main() {
 	router := gin.Default()
 
-	router.GET("/hello-world", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "Hello, World!",
-		})
-	})
+	router.GET("/products/:id", handler)
 
 	router.Run()
 }
